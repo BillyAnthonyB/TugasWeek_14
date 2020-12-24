16d90e1c445454143759bb82d006b5a14c0c7f30
BillyAnthonyB <BillyAnthonyB@github.com>
2020-12-25 01:51:10 +0700

    Add project files.

---
+++ TugasWeek_14/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TugasWeek_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelKalimat = new System.Windows.Forms.Label();
+            this.labelHuruf = new System.Windows.Forms.Label();
+            this.textBoxKalimat = new System.Windows.Forms.TextBox();
+            this.textBoxHuruf = new System.Windows.Forms.TextBox();
+            this.labelMenjadi = new System.Windows.Forms.Label();
+            this.textBoxMenjadi = new System.Windows.Forms.TextBox();
+            this.buttonKonversi = new System.Windows.Forms.Button();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.pictureBoxPanda = new System.Windows.Forms.PictureBox();
+            this.labelCoba = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPanda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelKalimat
+            // 
+            this.labelKalimat.AutoSize = true;
+            this.labelKalimat.Location = new System.Drawing.Point(40, 47);
+            this.labelKalimat.Name = "labelKalimat";
+            this.labelKalimat.Size = new System.Drawing.Size(94, 13);
+            this.labelKalimat.TabIndex = 0;
+            this.labelKalimat.Text = "Masukkan Kalimat";
+            // 
+            // labelHuruf
+            // 
+            this.labelHuruf.AutoSize = true;
+            this.labelHuruf.Location = new System.Drawing.Point(40, 87);
+            this.labelHuruf.Name = "labelHuruf";
+            this.labelHuruf.Size = new System.Drawing.Size(86, 13);
+            this.labelHuruf.TabIndex = 1;
+            this.labelHuruf.Text = "Masukkan Huruf";
+            // 
+            // textBoxKalimat
+            // 
+            this.textBoxKalimat.Location = new System.Drawing.Point(150, 47);
+            this.textBoxKalimat.Name = "textBoxKalimat";
+            this.textBoxKalimat.Size = new System.Drawing.Size(288, 20);
+            this.textBoxKalimat.TabIndex = 2;
+            // 
+            // textBoxHuruf
+            // 
+            this.textBoxHuruf.Location = new System.Drawing.Point(150, 85);
+            this.textBoxHuruf.Name = "textBoxHuruf";
+            this.textBoxHuruf.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHuruf.TabIndex = 3;
+            // 
+            // labelMenjadi
+            // 
+            this.labelMenjadi.AutoSize = true;
+            this.labelMenjadi.Location = new System.Drawing.Point(279, 88);
+            this.labelMenjadi.Name = "labelMenjadi";
+            this.labelMenjadi.Size = new System.Drawing.Size(44, 13);
+            this.labelMenjadi.TabIndex = 4;
+            this.labelMenjadi.Text = "Menjadi";
+            // 
+            // textBoxMenjadi
+            // 
+            this.textBoxMenjadi.Location = new System.Drawing.Point(338, 84);
+            this.textBoxMenjadi.Name = "textBoxMenjadi";
+            this.textBoxMenjadi.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMenjadi.TabIndex = 5;
+            // 
+            // buttonKonversi
+            // 
+            this.buttonKonversi.Location = new System.Drawing.Point(198, 123);
+            this.buttonKonversi.Name = "buttonKonversi";
+            this.buttonKonversi.Size = new System.Drawing.Size(158, 23);
+            this.buttonKonversi.TabIndex = 6;
+            this.buttonKonversi.Text = "Konversi!";
+            this.buttonKonversi.UseVisualStyleBackColor = true;
+            this.buttonKonversi.Click += new System.EventHandler(this.buttonKonversi_Click);
+            // 
+            // labelHasil
+            // 
+            this.labelHasil.AutoSize = true;
+            this.labelHasil.Location = new System.Drawing.Point(40, 174);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(39, 13);
+            this.labelHasil.TabIndex = 7;
+            this.labelHasil.Text = "Hasil : ";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(163, 174);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 13);
+            this.labelOutput.TabIndex = 8;
+            // 
+            // pictureBoxPanda
+            // 
+            this.pictureBoxPanda.Image = global::TugasWeek_14.Properties.Resources._185_1855413_panpan_pandabears_panda_webarebears_we_bare_bears_pan;
+            this.pictureBoxPanda.Location = new System.Drawing.Point(591, 241);
+            this.pictureBoxPanda.Name = "pictureBoxPanda";
+            this.pictureBoxPanda.Size = new System.Drawing.Size(219, 227);
+            this.pictureBoxPanda.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPanda.TabIndex = 9;
+            this.pictureBoxPanda.TabStop = false;
+            // 
+            // labelCoba
+            // 
+            this.labelCoba.AutoSize = true;
+            this.labelCoba.Location = new System.Drawing.Point(122, 174);
+            this.labelCoba.Name = "labelCoba";
+            this.labelCoba.Size = new System.Drawing.Size(52, 13);
+            this.labelCoba.TabIndex = 10;
+            this.labelCoba.Text = "OUTPUT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCoba);
+            this.Controls.Add(this.pictureBoxPanda);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.buttonKonversi);
+            this.Controls.Add(this.textBoxMenjadi);
+            this.Controls.Add(this.labelMenjadi);
+            this.Controls.Add(this.textBoxHuruf);
+            this.Controls.Add(this.textBoxKalimat);
+            this.Controls.Add(this.labelHuruf);
+            this.Controls.Add(this.labelKalimat);
+            this.Name = "Form1";
+            this.Text = "QUIZ PANDA";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPanda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelKalimat;
+        private System.Windows.Forms.Label labelHuruf;
+        private System.Windows.Forms.TextBox textBoxKalimat;
+        private System.Windows.Forms.TextBox textBoxHuruf;
+        private System.Windows.Forms.Label labelMenjadi;
+        private System.Windows.Forms.TextBox textBoxMenjadi;
+        private System.Windows.Forms.Button buttonKonversi;
+        private System.Windows.Forms.Label labelHasil;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.PictureBox pictureBoxPanda;
+        private System.Windows.Forms.Label labelCoba;
+    }
+}
+
---
+++ TugasWeek_14/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasWeek_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKonversi_Click(object sender, EventArgs e)
+        {
+            string NAMA = textBoxKalimat.Text.ToUpper();
+            int key = 0;
+            string A = textBoxHuruf.Text;
+            string B = textBoxMenjadi.Text;
+            char[] HURUF = A.ToCharArray();
+            char[] MENJADI = B.ToCharArray();
+            char[] URUTAN = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] KALIMAT = NAMA.ToCharArray();
+            labelCoba.Text = "";
+            if (HURUF[0] < MENJADI[0])
+            {
+                key = MENJADI[0] + HURUF[0];
+            }
+            else if (HURUF[0] > MENJADI[0])
+            {
+                key = HURUF[0] - MENJADI[0];
+            }
+            for (int i = 0; i < URUTAN.Length; i++)
+            {
+                for (int j = 0; j < KALIMAT.Length; j++)
+                {
+                    if (URUTAN[i] == KALIMAT[j])
+                    {
+                        KALIMAT[j] = URUTAN[(i+key) % 26];
+                    }
+                }
+            }
+            labelCoba.Text = new string(KALIMAT);
+        }
+    }
+}
